4d3e3f75d01fcafa3e55b6bfca8e64a61b966556
Grady Hilgendorf <rockenswim@yahoo.com>
2017-05-11 09:35:57 -0500

    asdf

--- phase4-node-express-framework/router/index.js
+++ phase4-node-express-framework/router/index.js
@@ -19,7 +19,7 @@ router.use(function(req, res, next) {  // req - request, res - response
 });
 
 // Route r1
-var r1 = router.route('/param_test');
+var r1 = router.route('/students');
 // /param_test?citizenship=United+States
 
 r1.all(function(req,res,next){
@@ -52,7 +52,7 @@ r1.get(function(req,res,next){  // req - request, res - response
 });
 
 // Route r3 illustrates a fully  paramterized GET route
-var r3 = router.route('/param_test');
+var r3 = router.route('/fte');
 // /param_test?citizenship=United+States&term_id=2
 
 r3.all(function(req,res,next){
